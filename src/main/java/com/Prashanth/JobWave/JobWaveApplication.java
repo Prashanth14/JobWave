07b2961bd110dce5de41290201502b720aa6fe7c
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-06-18 15:14:08 -0400

    Initial Commit

---
+++ src/main/java/com/Prashanth/JobWave/JobWaveApplication.java
@@ -0,0 +1,13 @@
+package com.Prashanth.JobWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Prashanth/JobWave/JobWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prashanth.JobWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
