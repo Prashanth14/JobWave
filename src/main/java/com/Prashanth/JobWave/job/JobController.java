c20a60ca2b77c4fe99b2d4355fbc2e70eb638405
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-06-18 22:32:25 -0400

    updated controllers with ResponseEntity

--- src/main/java/com/Prashanth/JobWave/job/JobController.java
+++ src/main/java/com/Prashanth/JobWave/job/JobController.java
@@ -1,5 +1,7 @@
 package com.Prashanth.JobWave.job;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,19 +15,19 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-       return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+       return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added Successfully";
+        return new ResponseEntity<>("Job added Successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id){
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
         Job job = jobService.getJobById(id);
-        return job;
+        return new ResponseEntity<>(job, HttpStatus.OK);
     }
 }
